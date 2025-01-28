7d477188a95ba87b83668d66d09945e104cd846a
AKADortys <thibault.a@outlook.com>
2025-01-29 00:39:38 +0100

    Ajout de seeding pour la bdd

--- src/database/mongo.provider.ts
+++ src/database/mongo.provider.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MongoClient, Db } from 'mongodb';
+import { data } from './mongo.seed-data';
 
 @Injectable()
 export class DatabaseProvider {
@@ -8,7 +9,7 @@ export class DatabaseProvider {
   private readonly databaseUri: string;
   private readonly databaseName: string;
   private client: MongoClient | null = null; // Instance partagée
-  private db: Db | null = null; // Instance de la base de données partagée
+  private db: Db | null = null;
 
   constructor(private configService: ConfigService) {
     this.databaseUri = this.configService.get<string>('MONGO_DB_URI');
@@ -25,6 +26,7 @@ export class DatabaseProvider {
         await this.client.connect();
         this.logger.warn('Connected to MongoDB');
         this.db = this.client.db(this.databaseName);
+        await this.seeding();
       } catch (error) {
         this.logger.error('Error connecting to MongoDB', error);
       }
@@ -34,12 +36,61 @@ export class DatabaseProvider {
   async getClient(): Promise<MongoClient> {
     return this.client;
   }
+
   async disconnect(): Promise<void> {
     if (this.client) {
       await this.client.close();
       this.client = null;
       this.db = null;
       this.logger.warn('Disconnected from MongoDB');
+    }
+  }
+  // Fonction de seeding des données de départ.
+  private async seeding(): Promise<void> {
+    if (!this.db) {
+      this.logger.error('Database instance is not available.');
+      return;
+    }
+
+    const collections = ['customers', 'products', 'orders'];
+
+    for (const collectionName of collections) {
+      try {
+        const collection = this.db.collection(collectionName);
+
+        // Vérifie si la collection est vide
+        const existingDataCount = await collection.countDocuments();
+        if (existingDataCount === 0) {
+          this.logger.log(`Seeding ${collectionName} collection...`);
+
+          // Insère les données de seeding
+          const seedData = this.getSeedData(collectionName);
+          await collection.insertMany(seedData);
+
+          this.logger.log(`Seeding ${collectionName} collection done.`);
+        } else {
+          this.logger.log(`${collectionName} collection already seeded.`);
+        }
+      } catch (error) {
+        this.logger.error(`Error seeding ${collectionName} collection:`, error);
+      }
+    }
+  }
+
+  //Retourne les données de seeding pour une collection donnée.
+  private getSeedData(collectionName: string): any[] {
+    switch (collectionName) {
+      case 'customers':
+        return data.customers;
+      case 'products':
+        return data.products;
+      case 'orders':
+        return data.orders;
+      default:
+        this.logger.warn(
+          `No seeding data found for collection: ${collectionName}`,
+        );
+        return [];
     }
   }
 }
---
+++ src/database/mongo.seed-data.ts
@@ -0,0 +1,167 @@
+export const data = {
+  customers: [
+    {
+      firstName: 'Christ',
+      lastName: 'Jesus',
+      email: 'sonOfGod@gmail.com',
+      password: 'Godiswithme',
+      role: 'admin',
+      phone: '0476123456',
+    },
+    {
+      firstName: 'Marie',
+      lastName: 'Magdeleine',
+      email: 'marie.magdeleine@example.com',
+      password: 'GraceAndPeace',
+      role: 'user',
+      phone: '0476234567',
+    },
+    {
+      firstName: 'Paul',
+      lastName: 'Apostle',
+      email: 'paul.apostle@example.com',
+      password: 'FaithHopeLove',
+      role: 'editor',
+      phone: '0476345678',
+    },
+    {
+      firstName: 'Pierre',
+      lastName: 'Saint',
+      email: 'pierre.rock@example.com',
+      password: 'KeysToHeaven',
+      role: 'admin',
+      phone: '0476456789',
+    },
+    {
+      firstName: 'Luc',
+      lastName: 'Evangelist',
+      email: 'luc.evangelist@example.com',
+      password: 'GoodNews',
+      role: 'user',
+      phone: '0476567890',
+    },
+    {
+      firstName: 'Jean',
+      lastName: 'Baptist',
+      email: 'john.baptist@example.com',
+      password: 'PrepareTheWay',
+      role: 'user',
+      phone: '0476678901',
+    },
+    {
+      firstName: 'Thomas',
+      lastName: 'Doubter',
+      email: 'thomas.doubt@example.com',
+      password: 'MyLordMyGod',
+      role: 'editor',
+      phone: '0476789012',
+    },
+    {
+      firstName: 'Judas',
+      lastName: 'Iscariot',
+      email: 'judas.iscariot@example.com',
+      password: '30SilverCoins',
+      role: 'user',
+      phone: '0476890123',
+    },
+    {
+      firstName: 'David',
+      lastName: 'Shepherd',
+      email: 'david.shepherd@example.com',
+      password: 'HeartAfterGod',
+      role: 'admin',
+      phone: '0476901234',
+    },
+    {
+      firstName: 'Elie',
+      lastName: 'Prophet',
+      email: 'elie.prophet@example.com',
+      password: 'FireFromHeaven',
+      role: 'user',
+      phone: '0477012345',
+    },
+  ],
+  //commandes
+  orders: [],
+  //
+  //produits 10 exmeples
+  products: [
+    {
+      label: 'Bœuf Bourguignon',
+      description:
+        'Bœuf tendre mijoté dans une sauce au vin rouge avec des légumes.',
+      price: 15.99,
+      stock: 601,
+      category: 'Plat principal',
+    },
+    {
+      label: 'Coq au Vin',
+      description:
+        'Poulet mijoté dans une sauce au vin rouge avec des champignons et lardons.',
+      price: 13.5,
+      stock: 320,
+      category: 'Plat principal',
+    },
+    {
+      label: 'Ratatouille',
+      description:
+        'Mélange savoureux de légumes méditerranéens cuits dans une sauce tomate.',
+      price: 10.99,
+      stock: 450,
+      category: 'Accompagnement',
+    },
+    {
+      label: 'Quiche Lorraine',
+      description: 'Tarte salée garnie de lardons, crème et fromage.',
+      price: 8.99,
+      stock: 280,
+      category: 'Entrée',
+    },
+    {
+      label: "Soupe à l'Oignon",
+      description:
+        'Délicieuse soupe traditionnelle avec des oignons caramélisés et du fromage fondu.',
+      price: 7.5,
+      stock: 150,
+      category: 'Entrée',
+    },
+    {
+      label: 'Tarte Tatin',
+      description:
+        'Dessert classique composé de pommes caramélisées sur une pâte croustillante.',
+      price: 6.99,
+      stock: 180,
+      category: 'Dessert',
+    },
+    {
+      label: 'Mousse au Chocolat',
+      description: 'Mousse légère et aérienne au chocolat noir.',
+      price: 5.99,
+      stock: 220,
+      category: 'Dessert',
+    },
+    {
+      label: 'Blanquette de Veau',
+      description:
+        "Plat mijoté de veau accompagné de légumes et d'une sauce crémeuse.",
+      price: 17.5,
+      stock: 400,
+      category: 'Plat principal',
+    },
+    {
+      label: 'Salade Niçoise',
+      description: 'Mélange frais de légumes, thon, œufs durs et olives.',
+      price: 12.99,
+      stock: 350,
+      category: 'Entrée',
+    },
+    {
+      label: 'Crème Brûlée',
+      description:
+        'Dessert onctueux à la vanille, avec une croûte de caramel croquant.',
+      price: 6.5,
+      stock: 210,
+      category: 'Dessert',
+    },
+  ],
+};
